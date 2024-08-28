0a0fa0588f8eebecb2638a4347855a5434b79b41
laufer2805 <p.hrsak@tuta.com>
2024-08-28 12:14:57 +0200

    Player prefab updated
    Animator removed because it was broken, animation clips remain.
    Movement, aiming and shooting implemented.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,45 +8,61 @@ public class Player : MonoBehaviour
 {
 	private Rigidbody2D rb;
 	private float horizontal;
+	private Vector2 mousePos;
 	private bool isGrounded;
 	[SerializeField] private float jumpHeight, movementSpeed;
 	[SerializeField] private LayerMask groundLayer;
-	[SerializeField] private Transform groundCheck;
-	[SerializeField] private Animator animator;
+	[SerializeField] private Transform groundCheck, gunTransform, gunBarrelTransform;
+	[SerializeField] private GameObject bulletPrefab;
+	[SerializeField] private Animation runRight;
+
+	private int isFlippedId, isMovingId;
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
 	}
 	private void Update()
 	{
-		rb.position += movementSpeed * horizontal * Time.deltaTime * Vector2.right;
-		
-		animator.SetFloat("moveDir", horizontal);
-		animator.SetFloat("jumpDir", rb.velocity.y);
+		rb.position += movementSpeed * horizontal * Time.deltaTime * Vector2.right;								// moving player according to horizontal input
+		if (horizontal > 0)
+		{
+			runRight.Play();
+		}
 	}
 	public void OnMoveInput (InputAction.CallbackContext context)
 	{
 		Debug.Log("Horizontal movement!");
-		horizontal = context.ReadValue<Vector2>().x;
+		horizontal = context.ReadValue<Vector2>().x;															// getting horizontal input
 	}
 	public void OnJumpInput (InputAction.CallbackContext context)
 	{
-		if (context.phase == InputActionPhase.Performed && isGrounded == true)
+		if (context.phase == InputActionPhase.Performed && isGrounded == true)									// getting jump input
 		{
 			Debug.Log("Jump!");
-			rb.AddForce(jumpHeight * Vector2.up, ForceMode2D.Impulse);
+			rb.AddForce(jumpHeight * Vector2.up, ForceMode2D.Impulse);											// adding jump force
 		}
-	}/*
-	private bool IsGrounded ()
+	}
+	public void OnMousePos (InputAction.CallbackContext context)
 	{
-		return Physics2D.OverlapCollider(groundCheckCollider, 0.2f, groundLayer);
-	}*/
+		mousePos = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());								// calculating world point of mouse
+		Vector2 temp = mousePos - (Vector2)gunTransform.position;												// calculating the vector between mousePos and gunPos
+		float rotationAngle = -1 * Vector2.SignedAngle(temp, Vector2.right);									// calculating rotation angle between vector above and x axis
+		gunTransform.rotation = Quaternion.Euler(0, 0, rotationAngle);                                          // rotating the gun
+	}
+	public void OnMouseShoot (InputAction.CallbackContext context)
+	{
+		if (context.phase == InputActionPhase.Performed)
+		{
+			GameObject bullet = Instantiate (bulletPrefab, gunBarrelTransform.position, gunTransform.rotation);	// instantiating the bullet at barrelPos and rotating it
+			Debug.Log("Shoot!");
+		}
+	}
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.CompareTag("Ground"))
 		{
 			Debug.Log("Grounded!");
-			isGrounded = true;
+			isGrounded = true;																					// grounding player
 		}
 	}
 	private void OnTriggerExit2D(Collider2D other)
@@ -54,7 +70,7 @@ public class Player : MonoBehaviour
 		if (other.gameObject.CompareTag("Ground"))
 		{
 			Debug.Log("UnGrounded!");
-			isGrounded = false;
+			isGrounded = false;																					// ungrounding player
 		}
 	}
 }

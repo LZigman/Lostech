e5584ff2bf843d87950645b6d5ac7ae90ac58dd0
laufer2805 <168451856+laufer2805@users.noreply.github.com>
2024-10-29 12:38:46 +0100

    audio manager implemented

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
+    [SerializeField] private AudioSource sfxAudioSource;
     [SerializeField] private Sound[] sounds;
+    [SerializeField] private GameObject audioSourcePrefab;
+    private AudioSource audioSource;
     private void Awake()
     {
         Instance = this;
+        audioSource = GetComponent<AudioSource>();
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject temp = Instantiate(audioSourcePrefab, transform);
+            temp.name = sounds[i].name;
+            temp.GetComponent<AudioSource>().clip = sounds[i].clip;
+        }
     }
 
-    public void PlaySound (string name)
+    public void PlaySFX (string name)
     {
-        if (sounds[0].name == name)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            sounds[0].source.Play();
+            if (transform.GetChild(i).name == name)
+            {
+                if (transform.GetChild(i).GetComponent<AudioSource>().isPlaying == false)
+                {
+                    transform.GetChild(i).GetComponent<AudioSource>().Play();
+                    return;
+                }
+            }
         }
+        Debug.Log("Sound not found!\nName: " + name);
     }
 }
--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+[System.Serializable]
 public class Sound
 {
     public string name;
     public AudioClip clip;
-    public AudioSource source;
 }
